bb8c38844b2942a3f7d5f56b1ae92a9978306ed4
agungsaputro62 <agungsaputroid62@gmail.com>
2021-12-26 13:01:01 +0700

    TEST

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -68,7 +68,7 @@ class ProductController extends Controller
         }
 
         return ResponseFormatter::success(
-                    // $product->paginate(100),
+                    $product->paginate($limit),
                     'Data Product Berhasil Diambil'
                 );
     }
