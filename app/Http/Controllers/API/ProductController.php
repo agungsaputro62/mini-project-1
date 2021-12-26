5a729e9ef930e584f0802aa4e76be37901be273e
agungsaputro62 <agungsaputroid62@gmail.com>
2021-12-26 13:33:08 +0700

    TEST

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -69,7 +69,7 @@ class ProductController extends Controller
 
         // Semoga Berhasil
         return ResponseFormatter::success(
-                    $product = Product::with('categories_id')->paginate(8),
+                    $product = Product::with('categories_id')->paginate(),
                     'Data Product Berhasil Diambil'
                 );
     }
