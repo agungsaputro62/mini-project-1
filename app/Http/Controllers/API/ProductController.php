408919f816c72c7cefd992d0bf7526138306e773
agungsaputro62 <agungsaputroid62@gmail.com>
2021-12-26 12:52:03 +0700

    Maju Tak Gentar Membela Yang Benar

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -28,7 +28,7 @@ class ProductController extends Controller
         $slug = $request->input('slug');
 
         if($id) {
-            $product = Product::with(['category', 'galleries'])->find($id);
+            $product = Product::with(['category'])->find($id);
 
             if($product) {
                 return ResponseFormatter::success(
@@ -45,7 +45,7 @@ class ProductController extends Controller
             }
         }
 
-        $product = Product::with(['category', 'galleries']);
+        $product = Product::with(['category']);
 
         if($name) {
             $product->where('name', 'like', '%' . $name . '%');
@@ -73,109 +73,109 @@ class ProductController extends Controller
                 );
     }
 
-    // Melihat Semua Data Product
-    public function index () {
+    // // Melihat Semua Data Product
+    // public function index () {
 
-        //
-        $products = Product::all();
+    //     //
+    //     $products = Product::all();
 
-        //
-        return response()->json(['products' => $products], 200);
+    //     //
+    //     return response()->json(['products' => $products], 200);
 
-    }
+    // }
 
-     // Melihat 1 Data Product Yang Kita Cari
-    public function show ($id) {
+    //  // Melihat 1 Data Product Yang Kita Cari
+    // public function show ($id) {
 
-        //
-        $products = Product::find($id);
+    //     //
+    //     $products = Product::find($id);
 
-        //
-        if($products) {
+    //     //
+    //     if($products) {
 
-            // Jika Ada Datanya
-            return response()->json(['products' => $products], 200);
-        }
-        else {
+    //         // Jika Ada Datanya
+    //         return response()->json(['products' => $products], 200);
+    //     }
+    //     else {
 
-            // No Found
-            return response()->json(['message' => 'Tidak Ada Datanya'], 404);
-        }
+    //         // No Found
+    //         return response()->json(['message' => 'Tidak Ada Datanya'], 404);
+    //     }
 
-        //
-        return response()->json(['products' => $products], 200);
+    //     //
+    //     return response()->json(['products' => $products], 200);
 
-    }
+    // }
 
-    // CREATE
-    public function store (ProductRequest $request) {
+    // // CREATE
+    // public function store (ProductRequest $request) {
 
-        //
-        $product = new Product;
-        $product->name = $request->name;
-        $product->description = $request->desc;
-        $product->price = $request->price;
-        $product->qty = $request->qty;
+    //     //
+    //     $product = new Product;
+    //     $product->name = $request->name;
+    //     $product->description = $request->desc;
+    //     $product->price = $request->price;
+    //     $product->qty = $request->qty;
 
-        //
-        $product->save();
+    //     //
+    //     $product->save();
 
-        //
-        return response()->json(['message' => 'Product Add Success'], 200);
+    //     //
+    //     return response()->json(['message' => 'Product Add Success'], 200);
 
-    }
+    // }
 
-    // UPDATE
-    public function update (ProductRequest $request, $id) {
+    // // UPDATE
+    // public function update (ProductRequest $request, $id) {
 
-        //
-        $product = Product::find($id);
+    //     //
+    //     $product = Product::find($id);
 
-        if ($product) {
+    //     if ($product) {
 
-            $product->name = $request->name;
-        $product->description = $request->desc;
-        $product->price = $request->price;
-        $product->qty = $request->qty;
+    //         $product->name = $request->name;
+    //     $product->description = $request->desc;
+    //     $product->price = $request->price;
+    //     $product->qty = $request->qty;
 
-        //
-        $product->update();
+    //     //
+    //     $product->update();
 
-        //
-        return response()->json(['message' => 'Product Update Success'], 200);
+    //     //
+    //     return response()->json(['message' => 'Product Update Success'], 200);
 
-        }
+    //     }
 
-        else {
+    //     else {
 
-            //
-            return response()->json(['message' => 'No Found Product'], 404);
+    //         //
+    //         return response()->json(['message' => 'No Found Product'], 404);
 
-        }
+    //     }
 
-    }
+    // }
 
-    // DELETE
-    public function delete ($id) {
+    // // DELETE
+    // public function delete ($id) {
 
-        $product = Product::find($id);
+    //     $product = Product::find($id);
 
-        if ($product) {
+    //     if ($product) {
 
-        //
-        $product->delete();
+    //     //
+    //     $product->delete();
 
-        //
-        return response()->json(['message' => 'Product Deleted Success'], 200);
+    //     //
+    //     return response()->json(['message' => 'Product Deleted Success'], 200);
 
-        }
+    //     }
 
-        else {
+    //     else {
 
-            //
-            return response()->json(['message' => 'No Found Product'], 404);
+    //         //
+    //         return response()->json(['message' => 'No Found Product'], 404);
 
-        }
+    //     }
 
-    }
+    // }
 }
